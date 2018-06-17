b5c9dd56cf0dc1f4667d1eab7b1d92c40f70d518
antixrist <ya.antixrist@gmail.com>
2018-06-17 12:05:28 +0400

    добавлено readme.md, приведены в порядок исходники

--- src/_bootstrap.js
+++ src/_bootstrap.js
@@ -1,8 +1,5 @@
 // import 'babel-polyfill';
 
-console.log('_bootstrap');
-
-/** nodemon */
 // process.once('SIGUSR2', function () {
 //   gracefulShutdown(function () {
 //     process.kill(process.pid, 'SIGUSR2');
--- src/asd/constant.js
+++
@@ -1,3 +0,0 @@
-import '_bootstrap';
-
-export default () => 321;
